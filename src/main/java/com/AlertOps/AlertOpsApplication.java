4c95fe8c8c0c6a6a014a187658921556d48554d7
Hemant <hk443957@gmail.com>
2025-08-25 13:23:59 +0530

    basic structure of project is comleted

---
+++ src/main/java/com/AlertOps/AlertOpsApplication.java
@@ -0,0 +1,13 @@
+package com.AlertOps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertOpsApplication.class, args);
+	}
+
+}
