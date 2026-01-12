70660f27c75da82cb54dc0d995352de0346fb85f
hemant <hk443957@gmail.com>
2026-01-12 23:34:05 +0530

    Defined esclation and flow_execution_state model

---
+++ src/main/java/com/alertops/flow_execution_engine/model/FlowExecutionState.java
@@ -0,0 +1,141 @@
+package com.alertops.flow_execution_engine.model;
+
+import java.math.BigInteger;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.UUID;
+import org.hibernate.annotations.UuidGenerator;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+
+@Entity
+public class FlowExecutionState {
+    @Id
+    @GeneratedValue
+    @UuidGenerator(style = UuidGenerator.Style.TIME)
+    private UUID id;
+
+    private String executionState;
+    private String notificationState;
+    private UUID taskId;
+    private String taskDetails;
+    private UUID nodeId;
+    private String userEmail;
+    private Duration duration;
+    private BigInteger position;
+    private UUID processId;
+    private Instant createdAt;
+    private Instant updatedAt;
+
+    @PrePersist
+    void onCreate() {
+        this.createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    void onUpdate() {
+        this.updatedAt = Instant.now();
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getExecutionState() {
+        return executionState;
+    }
+
+    public void setExecutionState(String executionState) {
+        this.executionState = executionState;
+    }
+
+    public String getNotificationState() {
+        return notificationState;
+    }
+
+    public void setNotificationState(String notificationState) {
+        this.notificationState = notificationState;
+    }
+
+    public UUID getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(UUID taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getTaskDetails() {
+        return taskDetails;
+    }
+
+    public void setTaskDetails(String taskDetails) {
+        this.taskDetails = taskDetails;
+    }
+
+    public UUID getNodeId() {
+        return nodeId;
+    }
+
+    public void setNodeId(UUID nodeId) {
+        this.nodeId = nodeId;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public Duration getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Duration duration) {
+        this.duration = duration;
+    }
+
+    public BigInteger getPosition() {
+        return position;
+    }
+
+    public void setPosition(BigInteger position) {
+        this.position = position;
+    }
+
+    public UUID getProcessId() {
+        return processId;
+    }
+
+    public void setProcessId(UUID processId) {
+        this.processId = processId;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    
+
+}
