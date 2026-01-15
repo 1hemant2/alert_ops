3830c3491d08a9dc010bcad6f67e51b2539f7faa
hemant <hk443957@gmail.com>
2026-01-16 00:03:35 +0530

    refactor packages name

--- src/main/java/com/alertops/flow_execution_engine/service/ReconcilerService.java
+++ src/main/java/com/alertops/flow_execution_engine/service/ReconcilerService.java
@@ -1,25 +1,30 @@
 package com.alertops.flow_execution_engine.service;
-/*
-package com.alertops.service;
+
 
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.alertops.dto.schedular.ScheduledEvent;
-import com.alertops.model.ScheduledTask;
-import com.alertops.repository.ScheduledTaskRepository;
+import com.alertops.flow_execution_engine.model.Escalation;
+import com.alertops.flow_execution_engine.model.FlowExecutionState;
+import com.alertops.flow_execution_engine.repository.EscalationRepository;
+import com.alertops.flow_execution_engine.repository.FlowExecutionStateRepository;
+import com.alertops.messaging.MessagePublisher;
 
 import java.util.List;
 
 
 @Service
 public class ReconcilerService {
-    private final ScheduledTaskRepository repo;
-    private final MessagePublisher publisher;
-    public ReconcilerService(ScheduledTaskRepository repo, MessagePublisher publisher) {
-        this.repo = repo;
-        this.publisher = publisher;
+    private final MessagePublisher messagePublisher;
+    private final EscalationRepository escalationRepository;
+    private final FlowExecutionStateRepository flowExecutionStateRepository;
+
+    public ReconcilerService(MessagePublisher messagePublisher, EscalationRepository escalationRepository, FlowExecutionStateRepository flowExecutionStateRepository) {
+        this.messagePublisher = messagePublisher;
+        this.escalationRepository = escalationRepository;
+        this.flowExecutionStateRepository = flowExecutionStateRepository;
     }
 
 
@@ -30,16 +35,20 @@ public class ReconcilerService {
 
     @Transactional
     public void reconcileOnce() {
-        List<ScheduledTask> pending = repo.findPendingToPublish(List.of("PENDING"));
+        List<Escalation> escalations = escalationRepository.findAllByStatus("RUNNING");        
 
-
-        for (ScheduledTask t : pending) {
-            Long delay =  t.getNextFireAt();
-            publisher.publishWithDelay(new ScheduledEvent(t.getId(), t.getEmail(), t.getMessage()), delay);
-            t.setQueued(true);
-            t.setStatus("QUEUED");
-            repo.save(t);
+        for(Escalation escalation : escalations) {
+            FlowExecutionState flowExecutionState = flowExecutionStateRepository
+                                                   .findFirstByProcessIdAndExecutionStateOrderByPositionAsc(escalation.getId(), "ACTIVE");
+            messagePublisher.publishWithDelay(flowExecutionState);
         }
+        // for (ScheduledTask t : pending) {
+        //     Long delay =  t.getNextFireAt();
+        //     publisher.publishWithDelay(new ScheduledEvent(t.getId(), t.getEmail(), t.getMessage()), delay);
+        //     t.setQueued(true);
+        //     t.setStatus("QUEUED");
+        //     repo.save(t);
+        // }
     }
 }
-*/
+
